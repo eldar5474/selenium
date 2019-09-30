5c489759ace6edf346c6243a2c2dde6c008c17eb
eldar <dr.shamhalov@mail.ru>
2019-09-30 11:19:16 +0300

    это ж пздц

--- src/test/java/InsuranseTest.java
+++ src/test/java/InsuranseTest.java
@@ -8,9 +8,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.sql.Driver;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class InsuranseTest {
@@ -36,24 +39,42 @@ public class InsuranseTest {
         Assert.assertEquals("Страхование путешественников", title.getText());
         driver.findElement(By.xpath("//IMG[@src='/portalserver/content/atom/contentRepository/content/person/travel/banner-zashita-traveler.jpg?id=f6c836e1-5c5c-4367-b0d0-bbfb96be9c53']")).click();
 
-        driver.findElement(By.xpath("(//DIV[@ng-click='setProdProg(prodProg)'])[1]")).click();
         String winHandleBefore = driver.getWindowHandle();
-        for(String winHandle : driver.getWindowHandles()){
+        for (String winHandle : driver.getWindowHandles()) {
             driver.switchTo().window(winHandle);
         }
-        driver.findElement(By.xpath("//INPUT[@type='checkbox'])[1]")).click();
 
-        //Wait<WebDriver> wait = new WebDriverWait(driver, 5, 1000);
-        //wait.until(ExpectedCondition.visibilityOf());
-        //driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-       // driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-       // JavascriptExecutor js = (JavascriptExecutor)driver;
-       // js.executeScript("scroll(0, 400);");
+        driver.findElement(By.xpath("(//DIV[@ng-click='setProdProg(prodProg)'])[1]")).click();
+        driver.findElement(By.xpath("//SPAN[@ng-click='save()'][text()='Оформить']")).click();
+        //Заполнение полей для "Застрахованные"
+        fillField(By.xpath("//INPUT[@name='insured0_surname']"), "Ivanov");
+        fillField(By.xpath("//INPUT[@name='insured0_name']"), "Ivan");
+        fillField(By.cssSelector("input[name=\"insured0_birthDate\"]"),"01011971");
 
-        //driver.findElement(By.xpath("(//INPUT[@type='checkbox'])[1]")).click();
-       // driver.findElement(By.xpath("(//DIV[@ng-click='setProdProg(prodProg)'])[1]")).click();
-       // driver.findElement(By.xpath("//SPAN[@ng-click='save()'][text()='Оформить']")).click();
+        //Заполнение полей страхователь
 
+        fillField(By.cssSelector("input[name=\"surname\"]"), "Смирнов");
+        fillField(By.cssSelector("input[name=\"name\"]"), "Сергей");
+        fillField(By.cssSelector("input[name=\"middlename\"]"), "Антонович");
+        fillField(By.cssSelector("input[name=\"birthDate\"]"),"02021972");
+
+        //Данные паспорта РФ
+        fillField(By.cssSelector("input[name=\"passport_series\"]"),"1234");
+        fillField(By.cssSelector("input[name=\"passport_number\"]"),"678901");
+        fillField(By.cssSelector("input[name=\"issueDate\"]"),"11112011");
+        fillField(By.cssSelector("textarea[name=\"issuePlace\"]"),"ТП УФМС РОССИИ по г.Москва");
+
+
+        //Продолжить
+        driver.findElement(By.cssSelector("span.b-continue-btn")).click();
+        WebElement fullness = driver.findElement(By.cssSelector("div[ng-show=\"tryNext && myForm.$invalid\"]"));
+        Assert.assertEquals("Заполнены не все обязательные поля", fullness.getText());
+
+
+    }
+    public void fillField(By locator, String value){
+        driver.findElement(locator).clear();
+        driver.findElement(locator).sendKeys(value);
     }
 
     @After
